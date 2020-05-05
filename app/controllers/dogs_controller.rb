9907c447265e3c9246ff2d9256b78b30d9f7bc71
jsabath <justin.sabath@aol.com>
2020-05-04 18:07:04 -0600

    1st Commit

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,11 @@
+class DogsController < ApplicationController
+    def index
+        if params[:search]
+            @dogs = Dog.where("name LIKE ?", "%#{params[:search]}%")
+            render json: @dogs
+        else
+            @dogs = Dog.all
+            render json: @dogs
+        end
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Dog.create({name: "Bixby"})
+Dog.create({name: "Fluffy"})
+Dog.create({name: "Max"})
+Dog.create({name: "Spot"})
+Dog.create({name: "Kodo"})
+Dog.create({name: "Bentley"})
